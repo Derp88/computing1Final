3eb73a617feed7726ec8d80bfc091d0f0f940237
Derp88 <62079853+Derp88@users.noreply.github.com>
2022-11-12 02:58:10 -0500

    Initial Form Work

--- bottle_app.py
+++ bottle_app.py
@@ -1,9 +1,20 @@
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route, template
 
+#Menu
 @route('/')
 def menu():
     return template("menu.html")
+
+#Page where user records match
+@route('/recordMatch')
+def recordMatch():
+    return template("recordMatchForm.html")
+
+#Page that confirms user input and saves data
+@post('/storeData')
+def storeData():
+    return template("storeData.html")
 
 application = default_app()
 
